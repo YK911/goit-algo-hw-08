022b082f2e26ed1ba642ad6c119ae4a8103bbb1e
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2024-01-31 13:08:35 +0200

    add total expenses for merging operations

--- task.py
+++ task.py
@@ -5,6 +5,7 @@ def connect_cables(cables):
     heap = cables[:]
     heapq.heapify(heap)
     visualize_connections = ""
+    expenses = 0
 
     while len(heap) > 1:
         cable1 = heapq.heappop(heap)
@@ -14,13 +15,14 @@ def connect_cables(cables):
         visualize_connections += f"({cable1[1]} > {cable2[1]}) { "=> " if len(heap) >= 1 else ""}" 
 
         heapq.heappush(heap, merged_cable)
+        expenses += merged_cable[0]
 
-    return heap[0], visualize_connections
+    return expenses, visualize_connections
 
 
 if __name__ == "__main__":
     cables = [(3, "A"), (5, "B"), (2, "C"), (8, "D"), (1, "E")]
-    result_heap, result_visualize = connect_cables(cables)
+    total_expenses, result_visualize = connect_cables(cables)
 
     print(f"Порядок об'єднання: {result_visualize}")
-    print(f"Загальні витрати: {result_heap[0]}")
+    print(f"Загальні витрати: {total_expenses}")
